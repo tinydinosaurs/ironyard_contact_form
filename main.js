8a2ad523a1a1241cdfd7767427500d946dbbe844
Dana <dana.lachman@gmail.com>
2016-03-21 21:21:44 -0500

    explorer mode complete, I think and hope

--- main.js
+++ main.js
@@ -8,28 +8,36 @@ var email = $('#email');
 var website = $('#website');
 var message = $('#message');
 
+// error message variables
+var nameError = $('#name_error');
+var emailError = $('#email_error');
+var websiteError = $('#website_error');
+var messageError = $('#message_error');
+
 function validate() {
+	// set initial html content to empty string
 	$('p').html('')
-	
+	// validate name input
 	if(userName.val() === '') {
-		$('#name_error').html('Name cannot be left empty');
+		nameError.html('Name cannot be left empty');
 	}
-
-	  if(email.val() === '') {
-		$('#email_error').html('Email cannot be left empty');
+	// validate email input
+	if(email.val() === '') {
+		emailError.html('Email cannot be left empty');
 	} else if(email.val().includes('@') === false) {
-		$('#email_error').html("Email must contain an '@'")
+		emailError.html("Email must contain an '@'")
 	} 
-
-	  if(website.val() === '') {
-		$('#website_error').html('Website cannot be left empty');
+	// validate website input
+	if(website.val() === '') {
+		websiteError.html('Website cannot be left empty');
 	} else if(website.val().includes('http://') === false) {
-		$('#website_error').html("Website must start with http://")
+		websiteError.html("Website must start with http://")
+	}
+	// validate message input
+	if(message.val() === '') {
+		messageError.html('Message cannot be left empty');
 	}
 
-	  if(message.val() === '') {
-		$('#message_error').html('Message cannot be left empty');
-	}
 }
 	
 
@@ -37,12 +45,23 @@ form.on('submit', function(e) {
 	e.preventDefault();
 	// create the function to handle the event
 	validate()
+	if(nameError.html() === '' && emailError.html() === '' && websiteError.html() === '' && messageError.html() === '') {
+		$('form').html('thanks for contacting us, ' + userName.val() + '. We have received your message and will be in touch with you shortly')
+	}
+
 });
 
 
+$('button').hover(
+	function() {
+	$(this).addClass('hover');
+	}, function() {
+		$(this).removeClass('hover');
+	}
+);
 
 
-////////// WHY U NO WORK WITH OTHER IF STATEMENTS?
+////////// WHY U NO WORK THE WAY I WANT U 2?
 	// inputField.each(function(index) {
 	// 	$('p').eq(index).html("");
 	// 	if($(this).val() === '') {
